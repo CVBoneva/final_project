338aa7d5fe28280f5e4197455ebbab986e7cd299
CVBoneva <cveta.boneva@gmail.com>
2024-06-06 16:11:19 +0200

    first commit

---
+++ project_file.py
@@ -0,0 +1,6 @@
+
+def even_numbers():
+    for i in range(10):
+        print(i*2 + 1)
+
+even_numbers()        
---
+++ project_file_2.py
@@ -0,0 +1,5 @@
+def convert_to_string(num):
+    return str(num)
+
+for i in range(10):
+    print("number=" + convert_to_string(i))
